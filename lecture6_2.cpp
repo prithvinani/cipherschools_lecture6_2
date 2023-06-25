badc08e0d4b97cdaaf27ee298e2df68ec21239a6
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-06-25 17:11:04 +0530

    Add files via upload

---
+++ lecture6_2.cpp
@@ -0,0 +1,40 @@
+// // #include <iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     for (int i = 1; i < 11; i++)
+// //     {
+// //         cout<<i;
+// //     }
+    
+// //     return 0;
+// // }
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int i ;
+//     i=0;
+//     int j,k;
+//     j=0;
+//     k=0;
+//     cout<<i<<" "<<j<<" "<<k<<endl;
+//     j = i++ ;
+//     cout<<i<<" "<<j<<" "<<k<<endl;
+//     k=++i;
+//     cout<<i<<" "<<j<<" "<<k<<endl;
+//     // cout<<++i<<endl;
+//     // cout<<i;
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;  ;i++)
+    {
+        //code block
+    }
+    return 0;
+}
